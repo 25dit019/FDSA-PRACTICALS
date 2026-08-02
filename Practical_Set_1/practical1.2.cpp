dfd8b24eada6eb6918ddaae8a96044a3014d7a2a
25dit019 <25dit019@charusat.edu.in>
2026-08-02 11:27:40 +0530

    Update practical1.2.cpp

--- Practical_Set_1/practical1.2.cpp
+++ Practical_Set_1/practical1.2.cpp
@@ -1,1 +1,56 @@
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter number of book records: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter Book IDs:\n";
+
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Book IDs borrowed more than once:\n";
+
+    for(int i=0; i<n; i++)
+    {
+        int count = 1;
+
+        bool printed = false;
+
+        for(int k=0; k<i; k++)
+        {
+            if(arr[i] == arr[k])
+            {
+                printed = true;
+                break;
+            }
+        }
+
+        if(printed)
+            continue;
+
+        for(int j=i+1; j<n; j++)
+        {
+            if(arr[i] == arr[j])
+            {
+                count++;
+            }
+        }
+
+        if(count > 1)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+
+    return 0;
+}
