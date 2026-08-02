88b843501419d2d3b3baf8c7f0616f8de196f925
25dit019 <25dit019@charusat.edu.in>
2026-08-02 11:27:15 +0530

    Update practical1.1.cpp

--- Practical_Set_1/practical1.1.cpp
+++ Practical_Set_1/practical1.1.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int n, h;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements:\n";
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter number of rotations: ";
+    cin >> h;
+
+    h = h % n;
+
+    for(int k=0; k<h; k++)
+    {
+        int first = arr[0];
+
+        for(int i=0; i<n-1; i++)
+        {
+            arr[i] = arr[i+1];
+        }
+
+        arr[n-1] = first;
+    }
+
+    cout << "Final Array:\n";
+
+    for(int i=0; i<n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
