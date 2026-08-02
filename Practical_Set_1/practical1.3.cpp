a0524e96b610aef6ee26a5f460df984e8882e05a
25dit019 <25dit019@charusat.edu.in>
2026-08-02 11:28:05 +0530

    Update practical1.3.cpp

--- Practical_Set_1/practical1.3.cpp
+++ Practical_Set_1/practical1.3.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main()
+{
+    string sentence;
+
+    cout << "Enter a sentence:\n";
+    getline(cin, sentence);
+
+    string word = "";
+    string longest = "";
+
+    for(int i=0; i<=sentence.length(); i++)
+    {
+        if(i == sentence.length() || sentence[i] == ' ')
+        {
+            if(word.length() > longest.length())
+            {
+                longest = word;
+            }
+
+            word = "";
+        }
+        else
+        {
+            word += sentence[i];
+        }
+    }
+
+    cout << "Longest Word: " << longest << endl;
+    cout << "Length: " << longest.length();
+
+    return 0;
+}
